c1f8cd181f60b8168eb18c8eb82960de89105a1d
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2024-02-07 14:54:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-# # Задание 1
 # a = int(input())
 # b = int(input())
 # c = int(input())
-#
-# if a == b and a == c and b == c:
-#     print(3)
-# elif a == b or b == c or a == c:
-#     print(2)
-# if a != b and a != c and b != c:
-#     print(0)
+# ma = 0
+# mi = 0
+
+# if a == b and b == c and a == c:
+#     print('Tri chisla ravni')
+# else:    
+#     if a >= b and a >= c:
+#         ma = a  
+#         if b <= c:
+#             mi = b
+#         else:
+#             mi = c
+
+#     elif b >= c and b >= a:
+#         ma = b
+#         if c <= a:
+#             mi = c
+#         else:
+#             mi = a
+
+#     elif c >= a and c >= b:
+#         ma = c
+#         if a <= b:
+#             mi = a
+#         else:
+#             mi = b
+#     print("Max", ma)
+#     print("Min", mi)
 
 
-# # Задание 2.1
 # n = int(input())
-# for i in range(1, n + 1):
+# for i in range(n, 0, -1):
 #     for j in range(1, i + 1):
 #         print(j, end=' ')
 #     print()
 
-# Задание 2.2
-n = int(input())
-for i in range(1, n+1):
-    print(' ' * (2*(n-i)+1), end=' ')
-    for j in range(i, 0, -1):
-        print(j, end=' ')
-    for k in range(2, i+1):
-        print(k, end=' ')
-    print()
+# n = int(input())
+# for i in range(n, 0, -1):
+#     print(' ' * (n-i), end='')
+#     for j in range(i, 0, -1):
+#         print(j, end='')
+#     for k in range(2, i+1):
+#         print(k, end='')
+#     print('\t')
